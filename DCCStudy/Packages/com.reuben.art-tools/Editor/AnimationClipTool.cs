c40bb922fee4094b8131f845dd2d69af35d6cb62
Reuben-PC <2824016820@qq.com>
2022-07-27 21:56:24 +0800

    unity

---
+++ DCCStudy/Packages/com.reuben.art-tools/Editor/AnimationClipTool.cs
@@ -0,0 +1,17 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Reuben.ArtTools.Editor
+{
+    public class AnimationClipTool
+    {
+        [Title("Animation Tool")]
+        [Button("test")]
+        void TestUse()
+        {
+            Debug.Log("Test!");
+        }
+    }
+}
---
+++ DCCStudy/Packages/com.reuben.art-tools/Editor/Menu.cs
@@ -0,0 +1,24 @@
+﻿using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+
+namespace Reuben.ArtTools.Editor
+{
+    public class MainMenuWindow: OdinMenuEditorWindow
+    {
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            var tree = new OdinMenuTree();
+            tree.Add("Animation Clip", new AnimationClipTool(), EditorIcons.House);
+            return tree;
+        }
+
+        [MenuItem("Tools/Art Tools")]
+        private static void OpenWindow()
+        {
+            var window = GetWindow<MainMenuWindow>();
+            window.position = GUIHelper.GetEditorWindowRect().AlignCenter(800, 600);
+        }
+    }
+}
